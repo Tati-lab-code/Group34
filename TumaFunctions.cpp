b65a97c87c875c09b05c9e6e3d000e8426f58af7
BBCharming <139962458+BBCharming@users.noreply.github.com>
2025-04-18 14:36:43 +0200

    Update TumaFunctions.cpp

--- TumaFunctions.cpp
+++ TumaFunctions.cpp
@@ -1,20 +1,11 @@
 #include<iostream>
 using namespace std;
 void menu();
-float product(float, float);
 int main()
 {
-    //menu();
-    cout<<product(4, 4);
+    menu();
 }
 void menu()
 {
-    cout<<"Pick a choice:\n1. Add record\n2. Search record\n3. Delete record\n4. Exit\n";
-}
-float product(float a, float b)
-{
-   // cout<<"Enter two numbers of your choice separated by a space: ";
-    //cin>>a>>b;
-    float product=a*b;
-    return product;
+    cout<<"Benjamin Changed the code\n";
 }
