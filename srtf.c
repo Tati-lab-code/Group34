b9c463c5efaf6369478b235c7df8ec9ec9f01126
BBCharming <139962458+BBCharming@users.noreply.github.com>
2025-04-28 12:01:15 +0200

    Update srtf.c

--- srtf.c
+++ srtf.c
@@ -25,28 +25,12 @@ int main(void)
    scanf("%i",&numberOfProcesses);
    for (int i = 0; i < numberOfProcesses; i++)
    {
-      processes[i].processId = 10100 + i;//deletemw
       printf("Enter Arrival Time Of Process %i :", i+1);
       scanf("%i", &processes[i].arrivalTime);
       printf("Enter Burst Time Of Process %i: ", i+1);
       scanf("%i", &processes[i].burstTime);
-//      processes[i].processId = 1010000 + i;//deletemw
    }
    //sorting/queueing proceses 
-   for (int i = 0; i < numberOfProcesses; i++)
-   {
-      int n = 0;
-      while ( n < numberOfProcesses)
-      {
-         if(processes[i].arrivalTime > processes[n].arrivalTime)
-         {
-            waitingQueue[i] = processes[n].processId;
-         }
-         n++;
-      }
-   }
-   for (int i = 0; i < 5; i++){
-   printf("wait %i = %ins\n", i,waitingQueue[i]);
-   }
+ 
 }
 
